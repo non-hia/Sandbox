09daacee6f9911cdb8e1fe1351fad6eb2d2693a6
non-hia <non.hia.dev@gmail.com>
2018-05-05 17:38:27 +0900

    Create test.py

---
+++ python/test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return 2 * x
+
+x = np.linspace(-3, 3, 10)
+
+plt.plot(x, f(x), color='red')
+plt.grid(True)
+plt.savefig('test.png')
