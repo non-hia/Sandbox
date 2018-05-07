49cd8439bc6e6541d23dff8008a49f05c7e6edf1
non-hia <non.hia.dev@gmail.com>
2018-05-07 17:44:19 +0900

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+print("テスト")
+
 def f(x):
-    return 2 * x
+    return 2 * x + 1
 
 x = np.linspace(-3, 3, 10)
 
 plt.plot(x, f(x), color='red')
 plt.grid(True)
 plt.savefig('png/test.png')
+
